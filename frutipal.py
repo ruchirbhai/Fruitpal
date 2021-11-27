9b025edb270a5d1b7b3d61f6b3a6e5efec7836ba
ruchir <ruchirbhai@gmail.com>
2021-11-26 17:07:10 -0800

    update with file parsing and get responses and transaction types

--- frutipal.py
+++ frutipal.py
@@ -1,23 +1,48 @@
 from draughtsman import parse
+import sys
 
-api = '# My AAPI'
-parse_result = parse(api)
-print(parse_result.api.title.defract)
+from refract.elements.primitives import String
 
-api1= """#FORMAT: 1A
-# # GET /commodities
-# + Response 200 (application/json)
+file = open("api-description.apib", 'r')
+parse_result = parse(file.read())
 
-# # GET /prices?commodity=pineapple&price_per_trade=100&volume_in_tons=1
+for elem in parse_result.api.content:
+    request = elem.transitions[0].transactions[0].request.defract
+    response = elem.transitions[0].transactions[0].response.defract
+    transaction_type = elem.transitions[0].transactions[0].request.element
+    endpoint = elem.href.defract
 
-# + Response 200 (application/json)
+    #print request and response
 
-#      {"PRICES":[{"COST":"102.58","COUNTRY":"BR"}]}
-"""
-parse_result = parse(api1)
-print(parse_result.api.title.defract)
-print(parse_result.api.defract)
+    print("transaction_type= ", transaction_type)
+    print("endpoint= ", endpoint)
+    print("Request= ", request)
+    print("Response= ", response)
+    
+# print("Basic API defract")
 
+# print(parse_result.api.defract)
+
+# # print("annotations")
+# # print(parse_result.annotations)
+# print("resources")
+# print(parse_result.api.resources)
+# print("content")
+# print(parse_result.content)
+# print("children")
+# print(parse_result.api.children.pop())
+# print("children")
+# print(parse_result.api.transitions)
+# print(parse_result.api.title.defract)
+# print("Title defract")
+# print(parse_result.api.title.attributes.get('sourcemap'))
+# print(parse_result.api.content.pop().defract)
+# print(parse_result.api.content.pop().defract)
+# elem1 = parse_result.api.content.pop(-1)
+# print(elem1.href.defract)
+# endpoint = []
+
+# print(endpoint)
 # FORMAT: 1A
 # GET /commodities
 # + Response 200 (application/json)
@@ -64,3 +89,10 @@ print(parse_result.api.defract)
 # + Response 200 (text/plain)
 
 #         Hello World! 
+
+# api = '# My AAPI git1'
+# # parse_result = parse(api)
+# # print(parse_result.api.title.defract)
+# api = '# My AAPI git'
+# parse_result = parse(api)
+# print(parse_result.api.title.defract)
