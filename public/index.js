0445317ad60281ac7e494db8e854346ae8b96f6a
ruchir <ruchirbhai@gmail.com>
2021-11-29 01:40:34 -0800

    added test cases for. missing values

--- public/index.js
+++ public/index.js
@@ -11,6 +11,17 @@ sio.on('connect', () => {
   sio.emit('priceAPI',mangoTest)
   sio.emit('priceAPI',appeleTest)
   sio.emit('priceAPI',pineapple)
+
+  // Wrong values
+  // commodity value missing
+  errorValTest1 = {'commodity': '', 'price_per_trade': '100', 'volume_in_tons': '1'}
+  sio.emit('priceAPI',errorValTest1)
+  // price_per_trade is not a number
+  errorValTest2 = {'commodity': 'mango', 'price_per_trade': 'hundred', 'volume_in_tons': '1'}
+  sio.emit('priceAPI',errorValTest2)
+  //volume_in_tons
+  errorValTest3 = {'commodity': 'mango', 'price_per_trade': '100', 'volume_in_tons': 'one'}
+  sio.emit('priceAPI',errorValTest3)
 });
 
 sio.on('disconnect', () => {
